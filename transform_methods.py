f44acf46378d1b4baa657e4375d7e17aa5093016
dylanchen0114 <dylanchen0114@gmail.com>
2017-10-04 19:36:20 +0800

    upload

--- transform_methods.py
+++ transform_methods.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import cross_val_score
 
